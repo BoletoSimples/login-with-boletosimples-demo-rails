c01c90f344abad651f1ffb6200e83ab7fe2db2d2
Thiago Belem <contato@thiagobelem.net>
2014-07-09 17:32:32 -0300

    Create Demo#logged_in to display user data

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,11 +1,15 @@
 class DemoController < ApplicationController
+  before_filter :authenticate!, only: [:logged_in]
+
   def index
   end
 
   def api
   end
-  
+
   def access_api
-    
+  end
+
+  def logged_in
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'demo/index'
   get 'demo/api'
-  post 'demo/access_api'  
+  post 'demo/access_api'
+  get 'demo/logged_in'
 
   get "/auth/boletosimples/callback", to: "sessions#create"
 
