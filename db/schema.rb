8bf5466ad043aacfd4664d15452fa6651bb8a3f0
Thiago Belem <contato@thiagobelem.net>
2014-07-10 18:06:32 -0300

    Add access_token to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140513131450) do
+ActiveRecord::Schema.define(version: 20140710205604) do
 
   create_table "users", force: true do |t|
     t.string   "uid"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140513131450) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "access_token"
   end
 
 end
