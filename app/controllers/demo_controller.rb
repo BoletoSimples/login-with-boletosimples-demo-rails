71ef469006628ca2b9319cc60e826e7b46e587fb
Dirceu Pauka Jr <dirceuu@gmail.com>
2014-07-10 17:38:05 -0300

    add in README about app access_token

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,5 +1,3 @@
-# APP_ID=18cd60aef456d06c99941b262cfc33e22f10e84d3e335a95d8535a12dc65bd03 APP_TOKEN=9bcc4c7d81395e0b6ed1b16da85ed64121d47b00e90fe6058e058d4010f64104 APP_ACCESS_TOKEN=1c1e46f9df5a3c69db0807c76b443ac3aad43df1b8670a7661e2e43994fe0555 rails s
-
 class DemoController < ApplicationController
   before_filter :authenticate!, only: [:logged_in]
 
